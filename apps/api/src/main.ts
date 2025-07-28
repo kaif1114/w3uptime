dcfe5107c4aab4b0f606c46191a1fa0c3297dc73
kaifmuhammad <kaif.m134@gmail.com>
2025-07-28 23:14:42 +0500

    Prisma setup

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,7 @@ import express from "express";
 // import { routes } from "./startup/routes.js";
 import dotenv from "dotenv";
 import "express-async-errors";
+import { prisma } from "db/client";
 
 dotenv.config();
 
@@ -14,8 +15,8 @@ const app = express();
 //   exposedHeaders: ["Authorization"]
 // }));
 // app.use(cookieParser());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // app.use(errorHandler);
 
 // database();
@@ -24,6 +25,19 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+//route to test prisma for fetching from database
+app.get("/users", async (req, res) => {
+  console.log("Getting users for testing");
+ try {
+  const users = await prisma.user.findMany();
+  console.log("Users", users);
+  res.json(users);
+ } catch (error) {
+  console.error(error);
+  res.status(500).json({ message: "Internal server error" });
+ }
 });
 
 
