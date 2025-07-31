aa17ce06f4bf8bc3e3e0555e4b99c7453f011156
kaifmuhammad <kaif.m134@gmail.com>
2025-07-31 20:40:47 +0500

    W3U-12 controller created for patch route

--- apps/api/src/controllers/monitor/monitor.controller.ts
+++ apps/api/src/controllers/monitor/monitor.controller.ts
@@ -7,6 +7,11 @@ const createMonitorSchema = z.object({
   url: z.url().min(1),
 });
 
+const patchMonitorSchema = z.object({
+  name: z.string().min(1),
+  url: z.url().min(1),
+});
+
 export async function createMonitor(req: Request, res: Response) {
   const validation = createMonitorSchema.safeParse(req.body);
 
@@ -65,6 +70,42 @@ export async function getMonitors(req: Request, res: Response) {
   });
 
   return res.status(200).json({ monitors });
+}
+
+export async function patchMonitor(req: Request, res: Response) {
+  const { monitorId } = req.params;
+  if (!monitorId) {
+    return res.status(400).json({ error: "Monitor ID is required" });
+  }
+
+  const validation = patchMonitorSchema.safeParse(req.body);
+
+  if (!validation.success) {
+    return res.status(400).json({ error: validation.error.message });
+  }
+
+  const { name, url } = validation.data;
+
+  const monitor = await prisma.monitor.update({
+    where: {
+      id: monitorId,
+      userId: req.user.id,
+    },
+    data: {
+      name,
+      url,
+    },
+  });
+
+  return res.status(200).json({
+    message: "Monitor updated successfully",
+    monitor: {
+      id: monitor.id,
+      name: monitor.name,
+      url: monitor.url,
+      createdAt: monitor.createdAt,
+    },
+  });
 }
 
 export async function deleteMonitor(req: Request, res: Response) {
