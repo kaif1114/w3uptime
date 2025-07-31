c4d400b2330a88968ddce6c82c4aba481a9d769b
kaifmuhammad <kaif.m134@gmail.com>
2025-07-31 23:36:56 +0500

    W3U-12
    Fixed the error where ts-node was not detecting the declaration by adding reference to it at top of main.ts file

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,3 +1,4 @@
+/// <reference path="./types/global.d.ts" />
 import express from "express";
 // import { database } from "./startup/database.js";
 import { routes } from "./startup/routes";
---
+++ apps/api/src/types/global.d.ts
@@ -0,0 +1,21 @@
+import * as express from "express";
+
+declare global {
+  namespace Express {
+    export interface Request {
+      user: {
+        id?: string;
+      };
+    }
+  }
+}
+
+// declare namespace Express{
+//   export interface Request{
+//     user:{
+//       id?: string
+//     }
+//   }
+// }
+
+export {}
