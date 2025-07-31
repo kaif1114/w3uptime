e3546effd1a8f18d6e972cd5e6d75073d8f75d07
kaifmuhammad <kaif.m134@gmail.com>
2025-07-31 20:37:04 +0500

    W3U-12 CRUD routes for monitor added

--- apps/api/src/controllers/monitor/monitor.controller.ts
+++ apps/api/src/controllers/monitor/monitor.controller.ts
@@ -16,22 +16,76 @@ export async function createMonitor(req: Request, res: Response) {
 
   const { name, url } = validation.data;
 
+  const monitor = await prisma.monitor.create({
+    data: {
+      name,
+      url,
+      userId: req.user.id!,
+    },
+  });
 
-    const monitor = await prisma.monitor.create({
-      data: {
-        name,
-        url,
-        userId: req.user.id!,
-      },
-    });
+  return res.status(201).json({
+    message: "Monitor created successfully",
+    monitor: {
+      id: monitor.id,
+      name: monitor.name,
+      url: monitor.url,
+      createdAt: monitor.createdAt,
+    },
+  });
+}
 
-    return res.status(201).json({
-      message: "Monitor created successfully",
-      monitor: {
-        id: monitor.id,
-        name: monitor.name,
-        url: monitor.url,
-        createdAt: monitor.createdAt,
-      },
-    });
+export async function getMonitor(req: Request, res: Response) {
+  const { id } = req.params;
+
+  const monitor = await prisma.monitor.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!monitor) {
+    return res.status(404).json({ error: "Monitor not found" });
+  }
+
+  return res.status(200).json({
+    id: monitor.id,
+    name: monitor.name,
+    url: monitor.url,
+    createdAt: monitor.createdAt,
+  });
+}
+
+export async function getMonitors(req: Request, res: Response) {
+  const { id: userId } = req.user;
+  const monitors = await prisma.monitor.findMany({
+    where: {
+      userId,
+    },
+  });
+
+  return res.status(200).json({ monitors });
+}
+
+export async function deleteMonitor(req: Request, res: Response) {
+  const { monitorId } = req.params;
+  if (!monitorId) {
+    return res.status(400).json({ error: "Monitor ID is required" });
+  }
+
+  const monitor = await prisma.monitor.findUnique({
+    where: {
+      id: monitorId,
+    },
+  });
+
+  if (!monitor) {
+    return res.status(404).json({ error: "Monitor not found" });
+  }
+  await prisma.monitor.delete({
+    where: {
+      id: monitorId,
+    },
+  });
+  return res.status(200).json({ message: "Monitor deleted successfully" });
 }
--- apps/api/src/routes/monitor.ts
+++ apps/api/src/routes/monitor.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import { createMonitor, getMonitor } from "../controllers/monitor/monitor.controller";
 
 const router = Router();
 
-router.post("/create", )
+
+router.post("/create", createMonitor)
+router.get("/:id", getMonitor)
 
 export default router
