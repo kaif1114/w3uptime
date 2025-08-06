d4775fe4b8b5269e3dc7e06df41bb942a3dea6e2
kaifmuhammad <kaif.m134@gmail.com>
2025-08-07 02:51:19 +0500

    W3U-33

--- apps/frontend/components/ui/app-sidebar.tsx
+++ apps/frontend/components/ui/app-sidebar.tsx
@@ -1,61 +1,61 @@
-
-"use client"
-import { AlertTriangle, BarChart3, Shield, Users, Icon as LucideIcon } from "lucide-react"
-import { Icon as TablerIcon } from "@tabler/icons-react"
-
-
-
-
+"use client";
 import {
-  IconInnerShadowTop
-} from "@tabler/icons-react"
-import * as React from "react"
+  IconSatellite,
+  Icon as TablerIcon
+} from "@tabler/icons-react";
 
-import { NavMain } from "@/components/ui/nav-main"
-import { NavSecondary } from "@/components/ui/nav-secondary"
-import { NavUser } from "@/components/ui/nav-user"
+
+import { IconInnerShadowTop } from "@tabler/icons-react";
+import * as React from "react";
+
+
+import { NavUser } from "@/components/ui/nav-user";
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-} from "@/components/ui/sidebar"
-
-
-
+} from "@/components/ui/sidebar";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+import { AlertTriangleIcon, ChartBar, Shield, Users } from "lucide-react";
 
 // Menu items.
 const items = [
   {
     title: "Monitors",
     url: "/monitors",
-    icon: IconInnerShadowTop as TablerIcon,
- 
+    icon: ChartBar,
   },
   {
     title: "Incidents",
     url: "/incidents",
-    icon: IconInnerShadowTop as TablerIcon,
-   
+    icon: AlertTriangleIcon
   },
   {
     title: "Status Pages",
     url: "/status-pages",
-    icon: IconInnerShadowTop as TablerIcon,
-  
+    icon: Shield,
   },
   {
     title: "Escalation Policies",
     url: "/escalation-policies",
-   
-    icon: IconInnerShadowTop as TablerIcon,
+    icon: Users,
   },
-]
+];
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const pathname =usePathname()
+  useEffect(() => {
+    console.log(pathname)
+  }, [pathname])
   return (
     <Sidebar collapsible="offcanvas" {...props}>
       <SidebarHeader>
@@ -63,27 +63,44 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           <SidebarMenuItem>
             <SidebarMenuButton
               asChild
-              className="data-[slot=sidebar-menu-button]:!p-1.5"
+              className="data-[slot=sidebar-menu-button]:!p-1.5 mb-4"
             >
               <a href="#">
-                <IconInnerShadowTop className="!size-5" />
-                <span className="text-base font-semibold">Acme Inc.</span>
+                <IconSatellite className="!size-8" />
+                <span className="text-2xl font-bold">W3Uptime</span>
               </a>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
       <SidebarContent>
-        <NavMain items={items} />
-        <NavSecondary items={items} className="mt-auto" />
+        {/* main */}
+        <SidebarGroup>
+          <SidebarGroupContent className="flex flex-col gap-2">
+           
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton tooltip={item.title} className={cn(pathname === item.url && "bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground")}>
+                    {item.icon && <item.icon />}
+                    <Link href={item.url}>{item.title}</Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+       
       </SidebarContent>
       <SidebarFooter>
-        <NavUser user={{
-          name: "John Doe",
-          email: "john.doe@example.com",
-          avatar: "https://github.com/shadcn.png",
-        }} />
+        <NavUser
+          user={{
+            name: "Muhammad Kaif",
+            email: "kaif@w3uptime.com",
+            avatar: "https://github.com/shadcn.png",
+          }}
+        />
       </SidebarFooter>
     </Sidebar>
-  )
+  );
 }
