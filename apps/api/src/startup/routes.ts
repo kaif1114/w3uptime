b144af6524b10de05cfbb95489966130b0166778
kaifmuhammad <kaif.m134@gmail.com>
2025-08-12 02:09:35 +0500

    W3U-35 Removed express

--- apps/api/src/startup/routes.ts
+++
@@ -1,7 +0,0 @@
-import { Express } from "express";
-import monitorRouter from "../routes/monitor";
-import authMiddleware  from "../middleware/auth";
-
-export function routes(app: Express) {
-  app.use("/monitor", authMiddleware, monitorRouter);
-}
