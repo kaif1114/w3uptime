c4e61682d2055e7dce96105bc52f3e54d30ef1c5
kaifmuhammad <kaif.m134@gmail.com>
2025-08-18 17:14:13 +0500

    W3U-67 Added shared types for MonitorTick batch communication

--- apps/data-ingestion/src/main.ts
+++ apps/data-ingestion/src/main.ts
@@ -2,3 +2,15 @@
 // This avoids TS1479 when importing an ESM package from a CJS module
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { prisma } = require("db/client");
+import express from "express";
+import z from "zod";
+
+const app = express();
+
+app.get("/ping", (req, res) => {
+  return res.status(200).json({ message: "pong" });
+})
+
+const batchRequestSchema = z.object({})
+
+app.post("/batch", (req, res) => {})
