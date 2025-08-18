b67f9053f6b1b1ae33d46752181656a3565d3449
kaifmuhammad <kaif.m134@gmail.com>
2025-08-18 17:04:19 +0500

    W3U-66 Service Initialized

---
+++ apps/data-ingestion/src/main.ts
@@ -0,0 +1,4 @@
+// Use CommonJS require so ts-node (CJS) resolves the CJS export of db/client
+// This avoids TS1479 when importing an ESM package from a CJS module
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { prisma } = require("db/client");
