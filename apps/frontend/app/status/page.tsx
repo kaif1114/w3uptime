4412d43ccc35e0f539932b34c545a6b1b2c4cc2a
TanveerHamza <hamzatanveer961@gmail.com>
2025-08-28 15:53:47 +0500

    W3U-82 pages created

---
+++ apps/frontend/app/status/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Status= () => {
+  return (
+    <div>
+      This will serve for anyone who wants to check if the api, website is working or status of website as suggested by the user of w3uptime to show
+    </div>
+  )
+}
+
+export default Status
